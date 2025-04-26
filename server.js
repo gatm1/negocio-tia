81b53c09d3e92cfda16fac53550ae5e4a1bcb275
gatm1 <gatm1@users.noreply.github.com>
2025-04-25 20:47:24 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const app = express();
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 3000;
 
 // Configuración de PostgreSQL
 const pool = new Pool({
